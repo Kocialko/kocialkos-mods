d15a4a9e217d0291b0dc85054eb0cd5850a95866
Akatsuki2555 <akatsuki2555@proton.me>
2024-06-24 21:04:06 +0200

    Merge FPS booster from private repo

--- FPSBooster/FPSBooster/FPSBooster.cs
+++ FPSBooster/FPSBooster/FPSBooster.cs
@@ -1,10 +1,7 @@
-﻿using MSCLoader;
-
-using System;
-
+﻿using System.Collections.Generic;
+using System.Linq;
+using MSCLoader;
 using UnityEngine;
-
-using Object = UnityEngine.Object;
 
 namespace FPS_Booster
 {
@@ -12,93 +9,79 @@ namespace FPS_Booster
     {
         public override string ID => "FPS_Booster";
         public override string Name => "FPS Booster";
-        public override string Author => "MrKocialko";
+        public override string Author => "MrKocialko, アカツキ2555";
         public override string Version => "1.0";
         public override string Description => "";
 
+        private string[] listOfObjectsToDelete = new string[]
+        {
+                "TRAIN", "JAIL", "CABIN", "RCO_RUSCKO12(270)", "LakeReflectionFPSCamera", "LakeReflectionRallyTVcam1",
+                "THEATRE", "LakeReflectioncam", "LakeSimpleReflectionSceneCamera", "MAP/wheat_silos", "MAP/LakeSmallBottom1",
+                "MAP/ChickenHouse", "MAP/PierStore", "BOAT", "MAP/LakeWaterColor", "MAP/HouseOld5", "MAP/BusStop",
+                "MAP/LakeSimple", "MAP/Buildings", "MAP/SwampColliders", "MAP/TrafficSigns", "MAP/WaterWell", "MAP/PierHome",
+                "MAP/LakeWaterUnder1", "MAP/LakeSound 2", "MAP/LakeSound", "MAP/BirdTower", "MAP/Greenhouse", "MAP/WaterCheck1",
+                "MAP/JailSafeTrigger", "MAP/SkijumpHill", "MAP/LakeNice", "MAP/PierRiver", "MAP/Factory", "MAP/LakeWaterUnder2",
+                "MISC", "MAP/MESH/FOLIAGE", "MAP/MESH/ROCKS", "MAP/MESH/LogWallLarge", "MAP/MESH/RAILROAD_TUNNEL",
+                "MAP/MESH/CHURCHWALL", "MAP/MESH/RAILROAD", "MAP/MESH/WHEAT", "MAP/MESH/LAKEBED", "MAP/MESH/TREES1_COLL",
+                "MAP/MESH/TREES2_COLL", "MAP/MESH/TREES3_COLL", "MAP/MESH/ELEC_POLES", "PERAJARVI/HouseBig",
+                "PERAJARVI/CHURCH", "PERAJARVI/HouseSmall3", "PERAJARVI/DustCloudLOD1", "PERAJARVI/HouseRinatama3",
+                "PERAJARVI/Old3", "PERAJARVI/TerraceHouse", "PERAJARVI/School", "PERAJARVI/HouseWhite", "PERAJARVI/HouseOld1",
+                "PERAJARVI/HouseRinatama2", "PERAJARVI/MessageBoard", "COPS", "HUMANS", "Skidmarks", "RYKIPOHJA", "SOCCER",
+                "NPC_CARS", "ElectricityBills", "PlayerWanted", "RALLY", "AnimalsMoose", "TRAFFIC"
+        };
+        private List<SettingsCheckBox> settings = new List<SettingsCheckBox>();
+
         public override void ModSetup()
         {
-            SetupFunction(Setup.Update, Mod_Update);
+            base.ModSetup();
+
             SetupFunction(Setup.OnLoad, Mod_OnLoad);
+        }
+
+        public override void ModSettings()
+        {
+            base.ModSettings();
+
+            foreach (var item in listOfObjectsToDelete)
+            {
+                settings.Add(Settings.AddCheckBox(this, item, item, true));
+            }
         }
 
         private void Mod_OnLoad()
         {
-            Object.Destroy(GameObject.Find("TRAIN"));
-            Object.Destroy(GameObject.Find("JAIL"));
-            Object.Destroy(GameObject.Find("CABIN"));
-            Object.Destroy(GameObject.Find("RCO_RUSCKO12(270)"));
-            Object.Destroy(GameObject.Find("LakeReflectionFPSCamera"));
-            Object.Destroy(GameObject.Find("LakeReflectionRallyTVcam1"));
-            Object.Destroy(GameObject.Find("THEATRE"));
-            Object.Destroy(GameObject.Find("LakeReflectioncam"));
-            Object.Destroy(GameObject.Find("LakeSimpleReflectionSceneCamera"));
-            Object.Destroy(GameObject.Find("MAP/wheat_silos"));
-            Object.Destroy(GameObject.Find("MAP/LakeSmallBottom1"));
-            Object.Destroy(GameObject.Find("MAP/ChickenHouse"));
-            Object.Destroy(GameObject.Find("MAP/PierStore"));
-            Object.Destroy(GameObject.Find("BOAT"));
-            Object.Destroy(GameObject.Find("MAP/LakeWaterColor"));
-            Object.Destroy(GameObject.Find("MAP/HouseOld5"));
-            Object.Destroy(GameObject.Find("MAP/BusStop"));
-            Object.Destroy(GameObject.Find("MAP/LakeSimple"));
-            Object.Destroy(GameObject.Find("MAP/Buildings"));
-            Object.Destroy(GameObject.Find("MAP/SwampColliders"));
-            Object.Destroy(GameObject.Find("MAP/TrafficSigns"));
-            Object.Destroy(GameObject.Find("MAP/WaterWell"));
-            Object.Destroy(GameObject.Find("MAP/PierHome"));
-            Object.Destroy(GameObject.Find("MAP/LakeWaterUnder1"));
-            Object.Destroy(GameObject.Find("MAP/LakeSound 2"));
-            Object.Destroy(GameObject.Find("MAP/LakeSound"));
-            Object.Destroy(GameObject.Find("MAP/BirdTower"));
-            Object.Destroy(GameObject.Find("MAP/Greenhouse"));
-            Object.Destroy(GameObject.Find("MAP/WaterCheck1"));
-            Object.Destroy(GameObject.Find("MAP/JailSafeTrigger"));
-            Object.Destroy(GameObject.Find("MAP/SkijumpHill"));
-            Object.Destroy(GameObject.Find("MAP/LakeNice"));
-            Object.Destroy(GameObject.Find("MAP/PierRiver"));
-            Object.Destroy(GameObject.Find("MAP/Factory"));
-            Object.Destroy(GameObject.Find("MAP/LakeWaterUnder2"));
-            Object.Destroy(GameObject.Find("MISC"));
-            Object.Destroy(GameObject.Find("MAP/MESH/FOLIAGE"));
-            Object.Destroy(GameObject.Find("MAP/MESH/ROCKS"));
-            Object.Destroy(GameObject.Find("MAP/MESH/LogWallLarge"));
-            Object.Destroy(GameObject.Find("MAP/MESH/RAILROAD_TUNNEL"));
-            Object.Destroy(GameObject.Find("MAP/MESH/CHURCHWALL"));
-            Object.Destroy(GameObject.Find("MAP/MESH/RAILROAD"));
-            Object.Destroy(GameObject.Find("MAP/MESH/WHEAT"));
-            Object.Destroy(GameObject.Find("MAP/MESH/LAKEBED"));
-            Object.Destroy(GameObject.Find("MAP/MESH/TREES1_COLL"));
-            Object.Destroy(GameObject.Find("MAP/MESH/TREES2_COLL"));
-            Object.Destroy(GameObject.Find("MAP/MESH/TREES3_COLL"));
-            Object.Destroy(GameObject.Find("MAP/MESH/ELEC_POLES"));
-            Object.Destroy(GameObject.Find("PERAJARVI/HouseBig"));
-            Object.Destroy(GameObject.Find("PERAJARVI/CHURCH"));
-            Object.Destroy(GameObject.Find("PERAJARVI/HouseSmall3"));
-            Object.Destroy(GameObject.Find("PERAJARVI/DustCloudLOD1"));
-            Object.Destroy(GameObject.Find("PERAJARVI/HouseRinatama3"));
-            Object.Destroy(GameObject.Find("PERAJARVI/Old3"));
-            Object.Destroy(GameObject.Find("PERAJARVI/TerraceHouse"));
-            Object.Destroy(GameObject.Find("PERAJARVI/School"));
-            Object.Destroy(GameObject.Find("PERAJARVI/HouseWhite"));
-            Object.Destroy(GameObject.Find("PERAJARVI/HouseOld1"));
-            Object.Destroy(GameObject.Find("PERAJARVI/HouseRinatama2"));
-            Object.Destroy(GameObject.Find("PERAJARVI/MessageBoard"));
+            DestroyListedObjects();
         }
 
-        private void Mod_Update()
+        private void DestroyListedObjects()
         {
-            Object.Destroy(GameObject.Find("COPS"));
-            Object.Destroy(GameObject.Find("HUMANS"));
-            Object.Destroy(GameObject.Find("Skidmarks"));
-            Object.Destroy(GameObject.Find("RYKIPOHJA"));
-            Object.Destroy(GameObject.Find("SOCCER"));
-            Object.Destroy(GameObject.Find("NPC_CARS"));
-            Object.Destroy(GameObject.Find("ElectricityBills"));
-            Object.Destroy(GameObject.Find("PlayerWanted"));
-            Object.Destroy(GameObject.Find("RALLY"));
-            Object.Destroy(GameObject.Find("AnimalsMoose"));
-            Object.Destroy(GameObject.Find("TRAFFIC"));
+            GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+            List<GameObject> objectsToDestroy = new List<GameObject>();
+
+            foreach (var setting in settings)
+            {
+                if (setting.GetValue())
+                {
+                    var item = setting.Instance.ID;
+                    var lastTwoElements = item.Split('/').Reverse().Take(2).Reverse().ToArray();
+
+                    var parent = lastTwoElements.Length == 2 ? lastTwoElements[0] : null;
+                    var name = lastTwoElements.Length == 2 ? lastTwoElements[1] : lastTwoElements[0];
+
+                    foreach (GameObject obj in allObjects)
+                    {
+                        if (obj.name == name && (parent == null || (obj.transform.parent != null && obj.transform.parent.name == parent)))
+                        {
+                            objectsToDestroy.Add(obj);
+                        }
+                    }
+                }
+            }
+
+            foreach (GameObject obj in objectsToDestroy)
+            {
+                Object.Destroy(obj);
+            }
         }
     }
 }
