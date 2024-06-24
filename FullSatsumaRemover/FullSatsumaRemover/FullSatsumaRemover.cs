f9301b026ee1c80ef31b0a66b42f1da03437f85a
Akatsuki2555 <akatsuki2555@proton.me>
2024-06-24 21:04:20 +0200

    Remove unneeded code from FullSatsumaRemover

--- FullSatsumaRemover/FullSatsumaRemover/FullSatsumaRemover.cs
+++ FullSatsumaRemover/FullSatsumaRemover/FullSatsumaRemover.cs
@@ -16,26 +16,6 @@ namespace Full_Satsuma_Remover
         public override string Version => "1.0"; // Version
         public override string Description => ""; // Short description of your mod
 
-        public override void ModSetup()
-        {
-            SetupFunction(Setup.OnMenuLoad, OnMenuLoad);
-            SetupFunction(Setup.OnNewGame, OnNewGame);
-            SetupFunction(Setup.PreLoad, PreLoad);
-            SetupFunction(Setup.OnLoad, OnLoad);
-            SetupFunction(Setup.PostLoad, PostLoad);
-            SetupFunction(Setup.OnSave, OnSave);
-            SetupFunction(Setup.OnGUI, OnGUI);
-            SetupFunction(Setup.Update, Update);
-            SetupFunction(Setup.FixedUpdate, FixedUpdate);
-            SetupFunction(Setup.ModSettings, ModSettings);
-        }
-
-        public override void ModSettings()
-        {
-            // All settings should be created here. 
-            // DO NOT put anything that isn't settings or keybinds in here!
-        }
-
         public override void OnMenuLoad()
         {
             Object.Destroy(GameObject.Find("CARPARTS)"));
@@ -52,14 +32,6 @@ namespace Full_Satsuma_Remover
             Object.Destroy(GameObject.Find("SATSUMA(557kg, 248)"));
             // Called once, when creating a new game. This is useful for deleting old mod saves
         }
-        public override void PreLoad()
-        {
-            Object.Destroy(GameObject.Find("CARPARTS)"));
-            Object.Destroy(GameObject.Find("Scene/Car"));
-            Object.Destroy(GameObject.Find("CAR"));
-            Object.Destroy(GameObject.Find("SATSUMA(557kg, 248)"));
-            // Called once, right after GAME scene loads but before the game is fully loaded
-        }
         public override void OnLoad()
         {
             Object.Destroy(GameObject.Find("CARPARTS)"));
@@ -67,47 +39,6 @@ namespace Full_Satsuma_Remover
             Object.Destroy(GameObject.Find("CAR"));
             Object.Destroy(GameObject.Find("SATSUMA(557kg, 248)"));
             // Called once, when mod is loading after game is fully loaded
-        }
-        public override void PostLoad()
-        {
-            Object.Destroy(GameObject.Find("CARPARTS)"));
-            Object.Destroy(GameObject.Find("Scene/Car"));
-            Object.Destroy(GameObject.Find("CAR"));
-            Object.Destroy(GameObject.Find("SATSUMA(557kg, 248)"));
-            // Called once, after all mods finished OnLoad
-        }
-        public override void OnSave()
-        {
-            Object.Destroy(GameObject.Find("CARPARTS)"));
-            Object.Destroy(GameObject.Find("Scene/Car"));
-            Object.Destroy(GameObject.Find("CAR"));
-            Object.Destroy(GameObject.Find("SATSUMA(557kg, 248)"));
-            // Called once, when save and quit
-            // Serialize your save file here.
-        }
-        public override void OnGUI()
-        {
-            Object.Destroy(GameObject.Find("CARPARTS)"));
-            Object.Destroy(GameObject.Find("Scene/Car"));
-            Object.Destroy(GameObject.Find("CAR"));
-            Object.Destroy(GameObject.Find("SATSUMA(557kg, 248)"));
-            // Draw unity OnGUI() here
-        }
-        public override void Update()
-        {
-            Object.Destroy(GameObject.Find("CARPARTS)"));
-            Object.Destroy(GameObject.Find("Scene/Car"));
-            Object.Destroy(GameObject.Find("CAR"));
-            Object.Destroy(GameObject.Find("SATSUMA(557kg, 248)"));
-            // Update is called once per frame
-        }
-        public override void FixedUpdate()
-        {
-            Object.Destroy(GameObject.Find("CARPARTS)"));
-            Object.Destroy(GameObject.Find("Scene/Car"));
-            Object.Destroy(GameObject.Find("CAR"));
-            Object.Destroy(GameObject.Find("SATSUMA(557kg, 248)"));
-            // FixedUpdate is called once per fixed frame
         }
     }
 }
