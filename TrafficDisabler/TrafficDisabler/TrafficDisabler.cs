483461019d118bdc91d9ec99428020fab61b25c2
Akatsuki2555 <akatsuki2555@proton.me>
2024-06-24 21:14:36 +0200

    Remove unneeded code and improve TrafficDisabler

--- TrafficDisabler/TrafficDisabler/TrafficDisabler.cs
+++ TrafficDisabler/TrafficDisabler/TrafficDisabler.cs
@@ -19,24 +19,14 @@ namespace traffic_disabler
         public override void ModSetup()
         {
             SetupFunction(Setup.OnLoad, OnLoad);
-            SetupFunction(Setup.Update, Update);
         }
 
         public override void OnLoad()
         {
             Object.Destroy(GameObject.Find("TRAFFIC"));
             Object.Destroy(GameObject.Find("NPC_CARS"));
-            Object.Destroy(GameObject.Find("RALLY/RallyCars"));
-            Object.Destroy(GameObject.Find("HUMANS/Randomizer"));
-            Object.Destroy(GameObject.Find("TRAIN"));
-            Object.Destroy(GameObject.Find("COPS"));
-        }
-        public override void Update()
-        {
-            Object.Destroy(GameObject.Find("TRAFFIC"));
-            Object.Destroy(GameObject.Find("NPC_CARS"));
-            Object.Destroy(GameObject.Find("RALLY/RallyCars"));
-            Object.Destroy(GameObject.Find("HUMANS/Randomizer"));
+            Object.Destroy(GameObject.Find("RALLY").transform.Find("RallyCars").gameObject);
+            Object.Destroy(GameObject.Find("HUMANS").transform.Find("Randomizer").gameObject);
             Object.Destroy(GameObject.Find("TRAIN"));
             Object.Destroy(GameObject.Find("COPS"));
         }
